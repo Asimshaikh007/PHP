f25826b530017100d88ada843e143ef51efbb1d3
Asimshaikh007 <shaikhasim6165@gmail.com>
2025-02-27 17:55:02 +0530

    first commit

---
+++ Asim Shaikh/01_Simple Tags/tags.php
@@ -0,0 +1,34 @@
+<?php
+echo"
+<html>
+<head>
+<title>Simple Tags</title>
+</head>
+<body>
+<h1>Simple Tags</h1>
+<h2>hiii</h2>
+<h3>hiii</h3>
+<h4>hiii</h4>
+<p>Lorem ipsum dolor sit amet.</p><br>
+hello<u>Welcome</u>to my page<br>
+Hey<mark>Its Me</mark><br>
+<del>Deleted</del><br>
+<ins>Inserted</ins><br>
+H<sub>2</sub>)<br>
+2<sup>2</sup><br>
+<blockquote>Blockquote</blockquote><br>
+<q>Quote</q><br>
+<code>Code</code><br>
+<kbd>Keyboard</kbd><br>
+<pre>Pre</pre><br>
+<em>Emphasis</em><br>
+<strong>Strong</strong><br>
+<samp>Sample</samp><br>
+<var>Variable</var><br>
+<time>Time</time><br>
+<dfn>Definition</dfn><br>
+
+</body>
+</html>
+";
+?>
---
+++ Asim Shaikh/0_Welcome/index.php
@@ -0,0 +1,15 @@
+<?php
+echo "Welcome To My First PHP Page";
+echo"
+<html>
+ <head>
+ <title>Hello World</title>
+ </head>
+ <body>
+    <h1>Hello World!</h1><br>
+    <h1>Welcome To PHP</h1><br>
+    <p>Lorem ipsum dolor sit amet.</p><br>
+ </body>
+ </html>
+ ";
+ ?>
